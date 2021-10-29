7cdf9db82f350bd959f5f532f7777e0ba1f14db3
Huy Nguyen <huyn.nguyen95@gmail.com>
2021-10-29 04:51:10 -0500

    change svg files to js for exports

---
+++ src/images/icons/arrows.js
@@ -0,0 +1,29 @@
+import React from 'core-js/library/fn/reflect/es7/metadata'
+
+export const ArrowDown = () => (
+  <svg
+    width="68"
+    height="68"
+    viewBox="0 0 68 68"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="34" cy="34" r="34" fill="#F45844" />
+    <path
+      d="M34.9714 19.4286L34.9714 48.5714M34.9714 48.5714L25.7428 40.0997M34.9714 48.5714L44.2 40.0997"
+      stroke="white"
+      strokeWidth="2"
+    />
+  </svg>
+)
+
+export const ArrowRight = () => (
+  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+    <path
+      d="M15 15L15 17C15 17.961 16.2234 18.3688 16.8 17.6L20.55 12.6C20.8167 12.2444 20.8167 11.7556 20.55 11.4L16.8 6.4C16.2234 5.63119 15 6.03899 15 7L15 11C15 11.5523 14.5523 12 14 12L3 12"
+      stroke="black"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
